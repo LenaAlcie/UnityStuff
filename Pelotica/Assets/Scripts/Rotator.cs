d4da8f6211009be38eb405384a8a5601a3e96e4d
LenaAlcie <ht222fd@student.lnu.se>
2017-08-23 17:27:51 +0200

    Pick up cubes

---
+++ Pelotica/Assets/Scripts/Rotator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+	/*A prefab is a template that contains a blueprint of a game object.
+     We can use it in our project. */
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(15,30,45) * Time.deltaTime );
+        /*smooth and frame rate independent. */
+	}
+}
