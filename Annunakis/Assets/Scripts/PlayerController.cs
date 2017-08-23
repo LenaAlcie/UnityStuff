46b05e19e2bab8af01a5cb57702fc12061ecd897
LenaAlcie <ht222fd@student.lnu.se>
2017-08-23 22:57:50 +0200

    Annunakis
    https://unity3d.com/es/learn/tutorials/projects/2d-ufo-tutorial

---
+++ Annunakis/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour{
+	public float speed;
+
+	private Rigidbody2D body2d;
+
+	void Start(){
+		body2d = GetComponent<Rigidbody2D> ();
+	}
+
+	void FixedUpdate(){
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+
+		body2d.AddForce (movement * speed);
+	}
+
+}
