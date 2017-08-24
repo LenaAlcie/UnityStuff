c6dbb5d13bd42506bce2ec10bce8abedd9e68f83
LenaAlcie <ht222fd@student.lnu.se>
2017-08-24 16:09:57 +0200

    Enemies

---
+++ TowerT/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+    public float speed;
+
+    private Transform target;
+
+    private int wavepointIndex = 0;
+
+    private float distanceDifferenceBetweenPoints = 0.4f;
+
+    private void Start()
+    {
+        target = WaypointController.points[0];
+
+    }
+
+    void Update()
+    {
+        Vector3 direction = target.position - transform.position;
+
+        transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+        /*time deltatime assures that the speed is not related with the framerate of your computer
+         La longitud del vector (modulo) al multiplicarlo por speed, el bicho avanzará esa distancia tantas veces
+         como la speed diga. Para que vaya mejor, usar direction.normalized, que te dará el vector en 1 "metro"/"coso". 
+         */
+
+        if (Vector3.Distance(transform.position, target.position) <= distanceDifferenceBetweenPoints )
+        {
+            GetNextWaypoint();
+        }
+    }
+
+    void GetNextWaypoint() {
+
+        int waypointsArrayLenght = WaypointController.points.Length - 1;
+
+        if (wavepointIndex >= waypointsArrayLenght)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        wavepointIndex++;
+
+        target = WaypointController.points[wavepointIndex];
+
+    }
+}
---
+++ TowerT/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaveSpawner : MonoBehaviour {
+
+    public Transform enemyPrefab;
+
+    public Transform spawnPoint;
+
+    public float timeBetweenWaves = 5f;
+
+    private float countdown = 2f; //2 seconds before it begins to spawn
+
+    private int waveIndex = 0;
+
+    private float waitSeconds = 0.5f;
+
+
+     void Update()
+    {
+        if (countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+    }
+
+    IEnumerator SpawnWave() {
+
+        waveIndex++;
+
+        for (int i = 0; i < waveIndex; i++)
+        {
+
+            SpawnEnemy();
+            yield return new WaitForSeconds(waitSeconds);
+        }
+        
+    }
+
+    void SpawnEnemy() {
+
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+            
+    }
+}
