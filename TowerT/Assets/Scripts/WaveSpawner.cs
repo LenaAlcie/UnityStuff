5679e8dcfa06f171d9f8be66e488687b1d5673a6
LenaAlcie <ht222fd@student.lnu.se>
2017-08-27 20:02:53 +0200

    Wave Spawner
    Spawn enemies and stop spawn.

--- TowerT/Assets/Scripts/WaveSpawner.cs
+++ TowerT/Assets/Scripts/WaveSpawner.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class WaveSpawner : MonoBehaviour {
 
@@ -7,24 +8,36 @@ public class WaveSpawner : MonoBehaviour {
 
     public Transform spawnPoint;
 
+    public Text waveCountdownText;
+    public Text waveCounter; 
+
     public float timeBetweenWaves = 5f;
 
     private float countdown = 2f; //2 seconds before it begins to spawn
 
     private int waveIndex = 0;
 
+    private int lastWaveIndex = 5;
+
     private float waitSeconds = 0.5f;
 
+ 
 
-     void Update()
+
+    void Update()
     {
         if (countdown <= 0f)
         {
             StartCoroutine(SpawnWave());
             countdown = timeBetweenWaves;
         }
+        
+        countdown -= Time.deltaTime;//amount time since the last time we draw a frame (1/sec)
 
-        countdown -= Time.deltaTime;
+        waveCountdownText.text = ("Time to next wave: " +  Mathf.Round(countdown).ToString() );
+
+        waveCounter.text = ("Wave number: " + waveIndex);
+
     }
 
     IEnumerator SpawnWave() {
@@ -35,14 +48,28 @@ public class WaveSpawner : MonoBehaviour {
         {
 
             SpawnEnemy();
+
             yield return new WaitForSeconds(waitSeconds);
+
+          
         }
-        
+
+        if (waveIndex >= lastWaveIndex)
+        {
+            StopWaves();
+        }
+       
     }
 
     void SpawnEnemy() {
 
         Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
             
+    }
+
+    private void StopWaves() {
+
+        waveCountdownText.text = ("Level finished");
+        enabled = false;
     }
 }
