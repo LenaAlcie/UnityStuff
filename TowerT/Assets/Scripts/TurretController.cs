ac8394227b9ded1db8e7a7c6f14904de59834185
LenaAlcie <ht222fd@student.lnu.se>
2017-08-27 21:18:26 +0200

    Turrets!

---
+++ TowerT/Assets/Scripts/TurretController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretController : MonoBehaviour {
+
+    public Transform target;
+
+    public float range = 15f;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+           
+    }
+}
+
+//https://www.youtube.com/watch?v=QKhn2kl9_8I&t=1429s
